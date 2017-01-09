96b9a985f6c560132fc5635823dbb09ea3bcb7ce
Michael Joyner (office computer) <michael@newsrx.com>
2017-01-09 12:13:42 -0500

    expose memofield block size to permit setting a much tighter packing for memo values when outputing a lot of data.

--- xBaseJ/src/org/xBaseJ/DBF.java
+++ xBaseJ/src/org/xBaseJ/DBF.java
@@ -121,6 +121,18 @@ public class DBF implements Closeable, HasSize {
 	protected byte MDX_exist = 0;
 	protected byte language = 0x03;
 
+	public void setMemoBlockSize(int memoBlockSize) {
+		if (dbtobj==null) {
+			return; //noop
+		}
+		dbtobj.memoBlockSize=memoBlockSize;
+	}
+	public int getMemoBlockSize() {
+		if (dbtobj==null) {
+			return -1;
+		}
+		return dbtobj.memoBlockSize;
+	}
 	public long memoLength() throws IOException {
 		if (dbtobj == null) {
 			return -1;
