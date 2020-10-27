29a0579e6c48acd4d5a88789e303e8051cf76d9c
Michael Conrad <michael@newsrx.com>
2020-10-27 16:31:44 -0400

    Add convenience data format conversion methods to DBFRecord

--- src/main/java/org/xBaseJ/DBFRecord.java
+++ src/main/java/org/xBaseJ/DBFRecord.java
@@ -1,6 +1,10 @@
 package org.xBaseJ;
 
 import java.io.IOException;
+import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeParseException;
 
 public interface DBFRecord {
 	int recno();
@@ -9,4 +13,24 @@ public interface DBFRecord {
 	void put(String field, String value) throws ArrayIndexOutOfBoundsException, xBaseJException, IOException;
 	void setDeleted(boolean deleted) throws xBaseJException, IOException;
 	boolean isDeleted() throws xBaseJException, IOException;
+	default LocalDate getLocalDate(String field) throws xBaseJException, IOException {
+		try {
+			return LocalDate.parse(get(field), DateTimeFormatter.BASIC_ISO_DATE);
+		} catch (DateTimeParseException e) {
+			return null;
+		}
+	}
+	default void put(String field, LocalDate value) throws ArrayIndexOutOfBoundsException, xBaseJException, IOException {
+		put(field, value.format(DateTimeFormatter.BASIC_ISO_DATE));
+	}
+	default BigDecimal getDecimal(String field) throws xBaseJException, IOException {
+		try {
+			return new BigDecimal(get(field));
+		} catch (NumberFormatException e) {
+			return null;
+		}
+	}
+	default void put(String field, BigDecimal value) throws ArrayIndexOutOfBoundsException, xBaseJException, IOException {
+		put(field, value.toPlainString());
+	}
 }
